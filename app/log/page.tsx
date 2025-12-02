864d8d4a21740d6894049ac18c37a07c8cef53b4
rkim900707 <rkim900707@gmail.com>
2025-12-02 16:03:26 +0900

    fix: mdx 정적으로 변경

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -1,8 +1,13 @@
 import { getAllMdxPosts } from "@/lib/mdxPosts";
 import WorkLogClient from "@/components/WorkLogClient";
 
+// 이 페이지는 무조건 정적으로 빌드해라
+export const dynamic = "force-static"
+
+// 만약 ISR로 10분마다 다시 생성하고 싶으면(선택):
+// export const revalidate = 600; // 600초 = 10분
+
 export default async function LogPage() {
     const mdxPosts = await getAllMdxPosts();
-
-    return <WorkLogClient initialMdxPosts={mdxPosts} query={""} />;
+    return <WorkLogClient initialMdxPosts={mdxPosts} query="" />;
 }
